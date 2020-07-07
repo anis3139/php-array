6923c88e4af0ed95e485a48b22f2dc5fa5ab193f
anis3139 <anis904692@gmail.com>
2020-07-07 14:06:43 +0600

    map, filter, walk

--- phppractice.php
+++ phppractice.php
@@ -183,11 +183,173 @@
 
 
 //array splice (modify main array)
-$fruits=['mango','banana','orange','jackfruits','pulm','apple'];
+// $fruits=['mango','banana','orange','jackfruits','pulm','apple'];
 
-$newfruits=array("dates","pineapple");
-$fruitsData=array_splice($fruits,2,-1,$newfruits);
-print_r($fruitsData);
-print_r($fruits);
-echo "\n";
+// $newfruits=array("dates","pineapple");
+// $fruitsData=array_splice($fruits,2,-1,$newfruits);
+// print_r($fruitsData);
+// print_r($fruits);
+// echo "\n";
 
+
+
+
+
+//array marge or +
+// $random=["a"=>10,"b"=>12,"c"=>14,10=>15,"d"=>16,"e"=>18,"f"=>20];
+// $r1=array_slice($random,0,2, true);
+// $r2=array_slice($random,3,null,true);
+// $r3=array("j"=>45,"k"=>12);
+
+// $randomData=$r1+$r3+$r2;
+
+// print_r($randomData);
+
+
+
+
+
+
+
+
+
+// sort and asort
+// $randoms=["a"=>10,"b"=>12,"c"=>14,10=>15,"d"=>16,"e"=>18,"f"=>20];
+// $random=[2,5,5,8,8,5,4,5,8];
+// //sort($random);
+// //print_r($random);
+
+// asort($random);
+// print_r($random);
+
+// $n=count($random);
+
+// // loop with changes data
+// foreach($random as $r){
+//     echo $r."\n";
+// }
+
+// // reverce sort
+// arsort($random);
+// print_r($random);
+
+
+
+// // Key sort
+// ksort($randoms);
+// print_r($randoms);
+
+
+
+// // Key reverce sort
+// krsort($randoms);
+// print_r($randoms);
+
+
+// //sort string and case insansative sort
+
+// $fruits=["apple","banana","Jackfruits","orange","Apple","Banana"];
+
+// sort($fruits, SORT_STRING | SORT_FLAG_CASE);
+
+// print_r($fruits);
+
+
+
+
+
+//Searce
+
+// $randoms=["a"=>10,"b"=>12,"c"=>14,10=>15,"d"=>16,"e"=>18,"f"=>20];
+// $random=[2,5,5,8,8,5,4,5,8];
+
+// if(in_array(4,$random)){
+//     echo "Result Found"."\n";
+// }else{
+//    echo "Result not found";
+// }
+
+// $offset=array_search(4,$random);
+// echo $offset."\n";
+
+
+// if(key_exists('e',$randoms)){
+//     echo "Key is exists";
+// }else{
+//    echo "Not found";
+// }
+
+
+
+// //searce in assoc and diff
+
+// $num1=['a'=>1,'b'=>2, 'c'=>3,'d'=>4,'e'=>5];
+// $num2=['a'=>2,'b'=>2,'f'=>3,'d'=>6,'e'=>9];
+
+// $common=array_intersect($num1,$num2);
+// print_r($common);
+
+// $commonassoc=array_intersect_assoc($num1,$num2);
+// print_r($commonassoc);
+
+// $diff=array_diff($num1,$num2);
+// print_r($diff);
+
+
+// $diff2=array_diff_assoc($num1,$num2);
+// print_r($diff2);
+
+
+
+
+
+
+
+
+
+//array walk, map, filter
+
+
+$someNumber=[1,2,3,4,5,6,7,8,9,10,11,12];
+
+function squire($n){
+    printf("This number %s multiple is %s", $n, $n*$n);
+    echo "\n";
+}
+
+array_walk($someNumber,'squire');
+
+
+
+function cube($n){
+    return $n*$n*$n;
+}
+
+$cubeNumber=array_map('cube',$someNumber);
+
+print_r($cubeNumber);
+
+function even($n){
+    return $n%2==0;
+}
+
+function odd($n){
+    return $n%2==1;
+}
+
+$evenNumber=array_filter($someNumber,'even');
+$oddNumber=array_filter($someNumber,'odd');
+
+print_r($evenNumber);
+print_r($oddNumber);
+
+
+$name=['jalal','kamal','arif','jamal','jashim','jubayer'];
+
+function nameFilter($nm){
+    return $nm[0]=='j';
+}
+
+$filterbyj=array_filter($name,'nameFilter');
+
+print_r($filterbyj);
